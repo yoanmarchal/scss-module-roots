08eda2d3c7398c31d57bd1bb604e2033bb657369
Yoan marchal <marchalyoan@gmail.com>
2015-09-13 12:38:08 +0200

    add isotope

--- js/main.js
+++ js/main.js
@@ -2,3 +2,35 @@
 $(".headroom").headroom({
 
 });
+
+
+function on_resize(c,t){
+  onresize=function(){
+    clearTimeout(t);
+    t=setTimeout(c,100);
+  };
+  return c ;
+}
+
+var $container = $('#isotope-gallery-container');
+var $filter = $('.filter');
+$(window).load(function () {
+    // Initialize Isotope
+    $container.isotope({
+        itemSelector: '.gallery-item-wrapper'
+    });
+    $('.filter a').click(function () {
+        var selector = $(this).attr('data-filter');
+        $container.isotope({ filter: selector });
+        return false;
+    });
+    $filter.find('a').click(function () {
+        var selector = $(this).attr('data-filter');
+        $filter.find('a').parent().removeClass('active');
+        $(this).parent().addClass('active');
+    });
+});
+
+on_resize(function() {
+   $container.isotope('layout');
+}); // these parenthesis does the trick
