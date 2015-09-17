6c458b5ebdeb7d1d45f5c34ea1b623e00c3ea5ef
Yoan marchal <marchalyoan@gmail.com>
2015-09-17 17:18:45 +0200

    add contact plus map component

--- js/main.js
+++ js/main.js
@@ -1,7 +1,40 @@
  "use strict";function countDown(e,t){function a(){var e=Math.floor(g/1e3%60),t=Math.floor(g/6e4%60),n=Math.floor(g/36e5%24),r=Math.floor(g/864e5);i.val(e).trigger("change"),f.val(t).trigger("change"),p.val(n).trigger("change"),s.val(r).trigger("change"),h.text(e),l.text(t),c.text(n),u.text(r),g-=1e3,setTimeout(a,1e3)}var n=e||"Jul 4, 2013",r=t||"Aug 15, 2013",o=Date.parse(r)-Date.parse(n),g=Date.parse(r)-new Date,i=$(".second"),f=$(".minute"),p=$(".hour"),s=$(".day").attr("data-max",Math.floor(o/864e5)),h=i.parent().find("span"),l=f.parent().find("span"),c=p.parent().find("span"),u=s.parent().find("span");a()}
 
+function initMap(elem) {
 
-   
+    if($(elem).length==0){
+        return 0;
+    };
+
+    //Map start init
+    var mapOptions = {
+        center: new google.maps.LatLng(51.5111507, -0.1239844),
+        zoom: 15,
+        zoomControl: true,
+        zoomControlOptions: {
+            style: google.maps.ZoomControlStyle.DEFAULT,
+        },
+        disableDoubleClickZoom: false,
+        mapTypeControl: true,
+        mapTypeControlOptions: {
+            style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
+        },
+        scaleControl: true,
+        scrollwheel: false,
+        streetViewControl: true,
+        draggable : true,
+        overviewMapControl: false,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        styles: [{stylers:[{saturation:-100},{gamma:1}]},{elementType:"labels.text.stroke",stylers:[{visibility:"off"}]},{featureType:"poi.business",elementType:"labels.text",stylers:[{visibility:"off"}]},{featureType:"poi.business",elementType:"labels.icon",stylers:[{visibility:"off"}]},{featureType:"poi.place_of_worship",elementType:"labels.text",stylers:[{visibility:"off"}]},{featureType:"poi.place_of_worship",elementType:"labels.icon",stylers:[{visibility:"off"}]},{featureType:"road",elementType:"geometry",stylers:[{visibility:"simplified"}]},{featureType:"water",stylers:[{visibility:"on"},{saturation:50},{gamma:0},{hue:"#50a5d1"}]},{featureType:"administrative.neighborhood",elementType:"labels.text.fill",stylers:[{color:"#333333"}]},{featureType:"road.local",elementType:"labels.text",stylers:[{weight:0.5},{color:"#333333"}]},{featureType:"transit.station",elementType:"labels.icon",stylers:[{gamma:1},{saturation:50}]}]
+        }
+                    
+    var map = new google.maps.Map(document.getElementById('map'),mapOptions);
+    var marker = new google.maps.Marker({
+        icon: 'images/map-pin.png',
+        map: map,
+        position: map.getCenter() 
+    });
+};
 
 
 
@@ -60,10 +93,12 @@
 
      /* -------- Gallery Popup -------- */
      $(document).ready(function() {
+        //google map
+        initMap(".map");
 
-         count(".coming-soon");    
+        count(".coming-soon");    
 
-         $('.gallery-zoom').magnificPopup({
+        $('.gallery-zoom').magnificPopup({
              type: 'image',
              mainClass: 'mfp-with-zoom', // this class is for CSS animation below
              zoom: {
@@ -80,9 +115,39 @@
                      // you don't need to add "opener" option if this code matches your needs, it's defailt one.
                      return openerElement.is('img') ? openerElement : openerElement.find('img');
                  }
-             }
+            }
 
-         });
+        });
+
+        // contact form
+        $('#contactform').submit(function(){
+
+                var action = $(this).attr('action');
+
+                $("#message").slideUp(500,function() {
+                $('#message').hide();
+
+                $.post(action, {
+                    name: $('#name').val(),
+                    email: $('#email').val(),
+                    phone: $('#phone').val(),
+                    comments: $('#comments').val(),
+                },
+                    function(data){
+                        document.getElementById('message').innerHTML = data;
+                        $('#message').slideDown('slow');
+                        $('#submit').removeAttr('disabled');
+                        if(data.match('success') != null) $('#contactform').slideUp('slow');
+
+                    }
+                );
+
+                });
+
+                return false;
+
+            });
+
      });
      // End Gallery Popup
 
@@ -112,3 +177,13 @@
 
 
 
+
+
+
+
+<script src="https://maps.google.com/maps/api/js?sensor=true"></script>
+    
+
+
+    
+    
