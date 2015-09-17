e62dced09b771c15729a2cf9fe9610e170466245
Yoan marchal <marchalyoan@gmail.com>
2015-09-17 17:56:48 +0200

    correct path of img

--- js/main.js
+++ js/main.js
@@ -30,7 +30,7 @@ function initMap(elem) {
                     
     var map = new google.maps.Map(document.getElementById('map'),mapOptions);
     var marker = new google.maps.Marker({
-        icon: 'images/map-pin.svg',
+        icon: 'img/map-pin.svg',
         map: map,
         position: map.getCenter() 
     });
