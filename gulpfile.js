06608ebd39e56cfea67be531d6dd860d3247de4d
Yoan marchal <marchalyoan@gmail.com>
2016-03-04 13:59:39 +0100

    lot of changes

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+var gulp = require('gulp'), 
+    sass = require('gulp-sass')
+    notify = require("gulp-notify") 
+    bower = require('gulp-bower');
+
+var config = {
+     sassPath: './resources/sass',
+     bowerDir: './bower_components' 
+}
+
+gulp.task('bower', function() { 
+    return bower()
+         .pipe(gulp.dest(config.bowerDir)) 
+});
+
+gulp.task('icons', function() { 
+    return gulp.src(config.bowerDir + '/fontawesome/fonts/**.*') 
+        .pipe(gulp.dest('./public/fonts')); 
+});
+
+gulp.task('sass', function () {
+  return gulp.src('./sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
+});
+
+
+// Rerun the task when a file changes
+ gulp.task('watch', function() {
+     gulp.watch(config.sassPath + '/**/*.scss', ['css']); 
+});
+
+  gulp.task('default', ['bower', 'icons', 'sass']);
