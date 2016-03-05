26ab1ce2ae9df971354b622a103e55c7701493d8
Yoan marchal <marchalyoan@gmail.com>
2016-03-05 12:21:02 +0100

    try different inclusion

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,13 @@
+'use strict';
+
 var gulp = require('gulp'), 
-    sass = require('gulp-sass')
-    notify = require("gulp-notify") 
-    bower = require('gulp-bower');
+    sass = require('gulp-sass');
 
 var config = {
-     sassPath: './resources/sass',
+     sassPath: './sass',
      bowerDir: './bower_components' 
-}
+};
 
-
-
-gulp.task('bower', function() { 
-    return bower()
-         .pipe(gulp.dest(config.bowerDir)) 
-});
 
 gulp.task('icons', function() { 
     return gulp.src(config.bowerDir + '/fontawesome/fonts/**.*') 
@@ -22,7 +16,8 @@ gulp.task('icons', function() { 
 
 gulp.task('sass', function () {
   return gulp.src('./sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
+    .pipe(
+      sass().on('error', sass.logError))
     .pipe(gulp.dest('./css'));
 });
 
@@ -33,7 +28,7 @@ gulp.task('sass:watch', function () {
 
 // Rerun the task when a file changes
  gulp.task('watch', function() {
-     gulp.watch(config.sassPath + '/**/*.scss', ['css']); 
+  gulp.watch(config.sassPath + '/**/*.scss', ['css']); 
 });
 
-  gulp.task('default', ['bower', 'icons', 'sass']);
+  gulp.task('default', ['icons', 'sass']);
